f3da0345f2d97dd03bf6fc9f35e82a3781a21b61
tayona_1057178 <1057178@my.scccd.edu>
2025-11-07 09:56:21 -0800

    Map Mechanic completed

--- main.cc
+++ main.cc
@@ -1,64 +1,92 @@
 //Fill out this comment with your names and which bullet points you did
-//Partners: Giulliana Tayona, 
+//Partners: Giulliana Tayona,
 //Bullet Points: 1
-//Extra Credit: 
-//URL to cover art and music: 
+//Extra Credit:
+//URL to cover art and music:
 
 #include "/public/read.h"
 #include "/public/colors.h"
 #include <iostream>
+#include <cctype>
 #include <vector>
 using namespace std;
 
 vector<string> worldMap = {
+	"--------",
+	"|  c   |",
+	"| t    |",
+	"|      |",
+	"|      |",
+	"|      |",
+	"|      |",
+	"--------"
+};
 
-	"------------------------",
-	"|                      |",
-	"|		    			|",
-	"|                      |",
-	"|                      |",
-    "|                      |",
-    "|                      |",
-	"------------------------"
+char getLocation(size_t row, size_t column) {
+	if (row >= worldMap.size()) return ' ';
+	if (column >= worldMap.at(row).size()) return ' ';
+	return worldMap.at(row).at(column);
 }
 
-char getLocation (size_t row, size_t column) {
-	if (row >= world_map.size()) return ' ';
-	if (col >= world_map.at(row).size()) return ' ';
-	return world_map.at(row).at(col);
+void setLocation(size_t row, size_t column, char c) {
+	if (row >= worldMap.size()) return;
+	if (column >= worldMap.at(row).size()) return;
+	worldMap.at(row).at(column) = c;
 }
 
-void setLocation (size_t row, size_t column) {
-	if (row >= world_map.size()) return ' ';
-    if (col >= world_map.at(row).size()) return ' ';
-	worldMap.at(row).at(c) = 'G'
-}
-
-void printMap (size_t playRow, size_t playColumn) {
+void printMap(size_t playRow, size_t playColumn) {
 	clearscreen();
+	movecursor(0, 0);
 	for (int i = 0; i < worldMap.size(); i++) { // rows
 		for (int j = 0; j < worldMap.size(); j++) { // columns
-			if (i = playRow and j = playColumn) 
+			if (i == playRow and j == playColumn)
 				cout << "G";
 			else cout << worldMap.at(i).at(j);
-		
 		}
-		
 		cout << endl;
 	}
-	
+
 }
 
 
 int main() {
 	const int ROWS = worldMap.size();
 	const int COLUMNS = worldMap.at(0).size();
-	int rows = ROWS/2; int columns = COLUMNS/2;
-	int prevRow = -1; int prevCol = -1; //previous positions
+	int rows = ROWS / 2;
+	int columns = COLUMNS / 2;
+	int prevRow = -1;
+	int prevCol = -1; //previous positions
 	set_raw_mode(true);
 	show_cursor(false);
 
-	
-	
+	while (true) {
+		int c = toupper(quick_read());
+		if (c == 'W' or c == UP_ARROW) rows--;
+		if (c == 'S' or c == DOWN_ARROW) rows++;
+		if (c == 'A' or c == LEFT_ARROW) columns--;
+		if (c == 'D' or c == RIGHT_ARROW) columns++;
+
+		if (!(rows == prevRow and columns == prevCol)) {
+			printMap(rows, columns);
+			prevRow = rows;
+			prevCol = columns;
+		}
+
+		if (getLocation(rows, columns) == 't') {
+			setLocation(rows, columns, ' ');
+			movecursor(ROWS + 2, 0);
+			cout << "Item pick up TEST\n";
+		}
+		if (getLocation(rows, columns) == 'c') {
+			movecursor(ROWS + 2, 0);
+			cout << "Completed Test\n";
+			break;
+		}
+
+
+	}
+	set_raw_mode(false);
+	clearscreen();
+
 
 }
