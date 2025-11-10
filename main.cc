263093e26c2d3a2f9293f83d43f8ba75ce934f3d
!weguez0411 <1066504@my.scccd.edu>
2025-11-10 13:52:15 -0800

    Interface?

--- main.cc
+++ main.cc
@@ -17,15 +17,31 @@ class Bosses {
 	int maxHealth;
 	int curHealth;
 	int ATTDamage;
+	string Name;
 //	int damageTaken; not sure if this is necessary
 	//initialization
   public:
+	int getHealth() const;
+	string getName() const;
+	int getAttDamage() const;
 	Bosses() {
 		maxHealth = 100;
 		ATTDamage = 20;
 		curHealth = maxHealth;
+		Name = "Noname";
 	}
 };
+
+int Bosses::getHealth() const {
+	return curHealth;
+}
+string Bosses::getName() const {
+	return Name;
+}
+int Bosses::getAttDamage() const {
+	return ATTDamage;
+}
+
 //Class for main character
 class playerCharacter {
 	//basic stats for main character
@@ -40,16 +56,16 @@ class playerCharacter {
   public:
 	string getName() const;
 	int getAttDamage() const;
-	int getMaxHealth() const;
+	int getHealth() const;
 
 	playerCharacter() {
 		maxHealth = 10;
 		attDamage = 10;
-		curHealth = maxHealth;
+		curHealth = 10;
 	}
 	playerCharacter(string name, int maxHealth, int attDamage) {
 		setName(name); //Set the player character's name
-		setMaxHealth(maxHealth); // Health
+		setHealth(maxHealth); // Health
 		setAttDamage(attDamage); // Attack damage
 	}
 
@@ -60,11 +76,12 @@ class playerCharacter {
 			name = newName;
 	}
 
-	void setMaxHealth(int newHealth) {
+	void setHealth(int newHealth) {
 		if (newHealth <= 0) {
 			throw runtime_error("Your health can't be lower than 1");
 		} else
 			maxHealth = newHealth;
+		curHealth = maxHealth;
 	}
 
 	void setAttDamage(int newDamage) {
@@ -81,9 +98,11 @@ string playerCharacter::getName() const {
 int playerCharacter::getAttDamage() const {
 	return attDamage;
 }
-int playerCharacter::getMaxHealth() const {
-	return maxHealth;
+int playerCharacter::getHealth() const {
+	return curHealth;
 }
+
+
 
 //Exit the game when called
 void OnDeath() {
@@ -92,24 +111,60 @@ void OnDeath() {
 }
 
 //Attack system.. not sure how to work with this yet.
-int OnAttack(int ATTDamage, int curHealth) {
-	if (ATTDamage > 0) {
-		if (curHealth == 0) {
-			OnDeath();
-		}
-		return curHealth - ATTDamage;
-	} else {
-		cout << ("You are doing nothing");
-		return 0;
+void OnAttack(int attDamage, const int& other) {
+
+	if (attDamage > 0) {
+		curHealth = curHealth - attDamage;
 	}
 
 }
 
 //For test purpose
 int main() {
+	int userhealth, userAtt;
+	string userName;
+	bool isCombat = true;
+
+	Bosses firstBoss;
+
+	cout << "Character stats" << endl;
+	//Declare playercharacter class
 	playerCharacter Mario;
 
-	Mario.setName("Mario");
-	Mario.setMaxHealth(0);
-	Mario.setAttDamage(0);
+	cout << "What's your name" << endl;
+	cin >> userName;
+	Mario.setName(userName);
+
+	cout << "Attack Damage" << endl;
+	cin >> userAtt;
+	Mario.setAttDamage(userAtt);
+
+	cout << "HP" << endl;
+	cin >> userhealth;
+	Mario.setHealth(userhealth);
+
+	//Combat System
+	while (isCombat) {
+		int i = 0;
+		cout << "1. Attack\n";
+		cout << "2. Dodge\n";
+		cout << "3. My Stats\n";
+		cout << "4. Enemy Stats\n";
+		cout << "-1 to run away\n";
+		cin >> i;
+
+		if (i == -1) {
+			isCombat = false;
+		}/* else if (i == 1) {
+			OnAttack(Mario.getAttDamage(), firstBoss.getHealth());
+		}*/ else if (i == 3) {
+			cout << "Name: " << Mario.getName() << endl;
+			cout << "Attack Damage: " << Mario.getAttDamage() << endl;
+			cout << "HP: " << Mario.getHealth() << endl;
+		} else if (i == 4) {
+			cout << "Name: " << firstBoss.getName() << endl;
+			cout << "Attack Damage: " << firstBoss.getAttDamage() << endl;
+			cout << "HP: " << firstBoss.getHealth() << endl;
+		}
+	}
 }
